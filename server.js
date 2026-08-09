a10cb636a7a9314715a883307bf5a1c60b585a72
immosur212-droid <immosur212@gmail.com>
2026-08-09 20:11:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const rateLimit = require('express-rate-limit');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-
+const cloudinary = require('cloudinary').v2;
+// Configuration Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 const app = express();
 const PORT = process.env.PORT || 10000;
 

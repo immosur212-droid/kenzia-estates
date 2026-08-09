ec523e7aa2a2a7d24c039e47c6b1cf8a4df8f037
immosur212-droid <immosur212@gmail.com>
2026-08-09 11:37:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -332,4 +332,4 @@ app.use((req, res, next) => {
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
-Add missing admin routes for properties and messages
+
